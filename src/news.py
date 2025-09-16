4a5e5bd2105bb5933ce2c04407ad3d2405db9249
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2025-09-16 12:04:40 -0300

    Feat: analysis automatization done. Fix: SQL queries fixed. TO DO: correct database ER model and fix collectPrices

--- src/news.py
+++ src/news.py
@@ -4,15 +4,19 @@ import os
 from crud import Connector
 from utils import assets, languages, initial_date, final_date
 from llm import analyze_sentiment
+import time
 
 load_dotenv(override=True)
 
 def collect_news():
     newsapi = NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
     all_articles = []
+    connector = Connector()
 
     for ticker in assets.values():
         for language in languages:
+            isNational = True if language == 'pt' else False
+
             all_articles = newsapi.get_everything(q=ticker,
                                                   from_param=initial_date,
                                                   to=final_date,
@@ -20,11 +24,16 @@ def collect_news():
                                                   sort_by='relevancy')
 
             for article in all_articles['articles']:
-                result = analyze_sentiment(news_title=article.title,
-                                           news_body=article.content,
-                                           stock_name=ticker)
-                result.replace('*', '')
+                for i in range(0, 5):
+                    sentiment = analyze_sentiment(news_title=article.title,
+                                               news_body=article.content,
+                                               stock_name=ticker)
+                    if sentiment != 'irrelevant':
+                        connector.insert_news(title=article.title,
+                                              body_text=article.content,
+                                              date=article.publishedAt,
+                                              isNational=isNational,
+                                              emotion=sentiment,
+                                              assetTicker=ticker)
 
-                
-        
-if __name__ == "__main__":
+                time.sleep(60)

677528fbb71c07a24b1a263cd7dbdf12821191f3
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2025-12-10 11:20:07 -0300

    Created github workflows

--- src/collectPrices.py
+++ src/collectPrices.py
@@ -1,21 +1,39 @@
 import yfinance as yf
-from crud import Connector
+import pandas as pd
+import os
+#from crud import Connector
 from utils import initial_date, final_date
 
-asset_ticker = {'Oil': 'CL=F', 'Soy': 'ZS=F', 'Gold': 'GC=F', 'USD/BRL': 'USDBRL=X', 'Banco do Brasil': 'BBAS3.SA', 'Marfrig': 'MRFG3.SA', 'SLC Agrícola': 'SLCE3.SA'}
+def collect_prices():
+    asset_ticker = {'Oil': 'CL=F', 'Soy': 'ZS=F', 'Gold': 'GC=F', 'USD/BRL': 'USDBRL=X', 'Banco do Brasil': 'BBAS3.SA', 'Marfrig': 'MRFG3.SA', 'SLC Agrícola': 'SLCE3.SA'}
 
-for asset_name, ticker in asset_ticker.items():
-    asset = yf.Ticker(ticker)
-    asset_price_history = asset.history(period="3mo")
+    file_exists = os.path.isfile('prices.csv')
+    for asset_name, ticker in asset_ticker.items():
+        asset = yf.Ticker(ticker)
+        asset_price_history = asset.history(period="3mo")
 
-    for row in asset_price_history.iterrows():
-        date = row[0].strftime('%Y-%m-%d')
-        
-        if date >= initial_date and date <= final_date:
-            opening = row[1]
-            high = row[2]
-            low = row[3]
-            closing = row[4]
-            volume = row[5]
-            
-            Connector().insert_price(date, opening, closing, high, low, volume, ticker)
+        for row in asset_price_history.iterrows():
+            date = row[0].strftime('%Y-%m-%d')
+
+            if date >= initial_date and date <= final_date:
+                opening = row[1]
+                high = row[2]
+                low = row[3]
+                closing = row[4]
+                volume = row[5]
+
+                price = pd.DataFrame({'date': date,
+                                      'opening': opening,
+                                      'closing': closing,
+                                      'high': high,
+                                      'low': low,
+                                      'volume': volume,
+                                      'assetTicker': ticker}
+                )
+
+                price.to_csv('prices.csv', mode='a', header=not file_exists, index=False)
+
+                #Connector().insert_price(date, opening, closing, high, low, volume, ticker)
+
+if __name__ == '__main__':
+    collect_prices()
--- src/news.py
+++ src/news.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
 import os
-from crud import Connector
+import pandas as pd
+#from crud import Connector
 from utils import assets, languages, initial_date, final_date
 from llm import analyze_sentiment
 import time
@@ -10,8 +11,9 @@ load_dotenv(override=True)
 
 def collect_news():
     newsapi = NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
+    file_exists = os.path.isfile('news.csv')
     all_articles = []
-    connector = Connector()
+    #connector = Connector()
 
     for ticker in assets.values():
         for language in languages:
@@ -28,12 +30,21 @@ def collect_news():
                                            news_body=article['content'],
                                            stock_name=ticker)
                 if sentiment != 'irrelevant':
-                    connector.insert_news(title=article['title'],
-                                          body_text=article['content'],
-                                          date=article['publishedAt'],
-                                          isNational=isNational,
-                                          emotion=sentiment,
-                                          assetTicker=ticker)
+                    news = pd.DataFrame({'title': article['title'],
+                                         'body_text': article['content'],
+                                         'date': article['publishedAt'],
+                                         'isNational': isNational,
+                                         'emotion': sentiment,
+                                         'assetTicker': ticker}
+                    )
+                    news.to_csv('news.csv', mode='a', header=not file_exists, index=False)
+                    
+                    #connector.insert_news(title=article['title'],
+                    #                      body_text=article['content'],
+                    #                      date=article['publishedAt'],
+                    #                      isNational=isNational,
+                    #                      emotion=sentiment,
+                    #                      assetTicker=ticker)
 
                 time.sleep(70)
 
